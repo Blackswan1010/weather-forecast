488d8575f06f338eaf55f1d38ea2b20c415595b0
Anthony Nguyen <anthonynguye56@gmail.com>
2023-10-10 04:39:09 -0700

    add bg-color to 5-day forecast, fixing eventlistener

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,16 @@
 var userInput = document.getElementById("input");
 var citiesDiv = document.getElementById("cities");
 var searchBtn = document.getElementById("searchBtn");
-var button = document.querySelector(".btn");
+var today = document.getElementById("today");
+var fiveDays = document.getElementById("5-day");
+var cityButton = document.querySelector(".city-buttons");
 var apiKey = 'c6d6f56d59b4c33747b4f2496da55c91';
 var city = ['atlanta', 'denver', 'seattle', 'san+francisco', 'orlando', 'new+york', 'chicago', 'austin'];
 
 
 // Rendering and dynamically appending locations under the search bar
 for (var i = 0; i < city.length; i++) {
-    var queryUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city[i] + '&appid=' + apiKey;
+    var queryUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city[i] + '&appid=' + apiKey + "&units=imperial";
     fetch(queryUrl)
         .then(function (response) {
             return response.json();
@@ -20,26 +22,35 @@ for (var i = 0; i < city.length; i++) {
             cityName.setAttribute("type", "button");
             cityName.textContent = data.city.name;
             citiesDiv.appendChild(cityName);
+
         })
     if (!localStorage.getItem(city[i])) {
         localStorage.setItem(city[i], queryUrl);
     }
 }
 
-function getWeatherInfo(event){
+
+cityButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
     var target = event.target;
-    fetch(localStorage.getItem(target.textContent))
-        .then(function (response){
-            return response.json();
-        })
-        .then(function (data){
-            var 
-        })
+    if (target.matches(".btn")) {
+        console.log(target.textContent);
+        fetch(localStorage.getItem(target.textContent.toLowerCase()))
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                today.children[0].textContent = data.city.name + "(" + data.list[0].dt_txt + ")";
+                today.children[1].textContent = "Tempurature: " + data.list[0].main.temp + "\u00B0F";
+                today.children[2].textContent = "Wind: " + data.list[0].wind.speed + " MPH";
+                today.children[3].textContent = "Humidity: " + data.list[0].main.humidity + " %";
 
-}
+                // console.log("clicked");
 
-button.addEventListener('click', getWeatherInfo);
+            })
+    }
 
-
+});
 
 // temp, wind, humidity, picture, date
